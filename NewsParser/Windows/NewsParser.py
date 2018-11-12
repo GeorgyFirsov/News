7f470e214bd76c3f5ff1d8232f88f13d614e3a0b
Kron610 <ger.korekov@mail.ru>
2018-11-12 20:12:28 +0300

    Parser update_02

--- NewsParser/Windows/NewsParser.py
+++ NewsParser/Windows/NewsParser.py
@@ -12,26 +12,10 @@ from user_agent import generate_user_agent
 from time import sleep
 import numpy as np
  
-def get_news(url, index):
-    proxies_list = [{'http':'http://'+i} for i in ['67.149.217.254:10200',
-                '64.20.74.24:45554','62.37.237.101:8080',
-                '180.234.206.77:8080',
-                '78.11.85.13:8080','109.188.81.101:8080',
-                '139.59.17.113:8080','191.179.147.46:11421',
-                '111.68.99.42:8080','80.241.219.66:3128',
-                '201.20.94.106:8080','216.229.120.173:45554',
-                '116.58.247.31:3128','103.9.115.142:3128',
-                '82.164.99.193:10200','80.188.79.138:8080',
-                '36.75.113.224:8080',
-                '1.20.204.163:8080','97.77.49.151:45554',
-                '178.54.44.24:8080',
-                '65.182.136.153:45554', '111.76.129.223:808',
-                 '203.142.81.205'+':'+'8080', 
-                '42.202.35.185'+':'+'8118', '189.16.249.114'+':'+'8080',
-                '66.162.122.24'+':'+'8080']]
+def get_news(url):
     sleep(np.random.random())
     r = requests.get(url,headers={'User-Agent': generate_user_agent()},
-                               proxies=proxies_list[index%len(proxies_list)])
+                               proxies=get_proxy())
     soup = BeautifulSoup(r.text, "html.parser")
     dives = soup.find('div', class_='js-section-content largeTitle').find_all('div', class_='articleItem')
     dates = []
@@ -54,31 +38,62 @@ def write_csv(data, name):
         writer = csv.writer(f)
         writer.writerow(data)
 
-def write_file(url, name, index):
-    data = get_news(url, index)
+def write_file(url, name):
+    data = new_get_news(url, name)
     for i in data:
         write_csv(i,name)
 
-def get_links(url, names):
-    links = []
+def get_proxy():
+    proxies_list = [{'http':'http://'+i} for i in ['67.149.217.254:10200',
+                '64.20.74.24:45554','62.37.237.101:8080',
+                '180.234.206.77:8080',
+                '78.11.85.13:8080','109.188.81.101:8080',
+                '139.59.17.113:8080','191.179.147.46:11421',
+                '111.68.99.42:8080','80.241.219.66:3128',
+                '201.20.94.106:8080','216.229.120.173:45554',
+                '116.58.247.31:3128','103.9.115.142:3128',
+                '82.164.99.193:10200','80.188.79.138:8080',
+                '36.75.113.224:8080',
+                '1.20.204.163:8080','97.77.49.151:45554',
+                '178.54.44.24:8080',
+                '65.182.136.153:45554', '111.76.129.223:808',
+                 '203.142.81.205'+':'+'8080', 
+                '42.202.35.185'+':'+'8118', '189.16.249.114'+':'+'8080',
+                '66.162.122.24'+':'+'8080']]
+    return proxies_list[np.random.randint(0, len(proxies_list))]
+
+def new_get_news(url, name):
     browser = Chrome(executable_path="chromedriver")
-    for name in names:
-        browser.get(url)
-        search_form = browser.find_element_by_xpath('''/html/body/div[5]/header/div[1]/div/div[3]/div[1]/input''')
-        search_form.send_keys(name)
-        search_form.send_keys(Keys.ENTER)
-        sleep(2)
-        news_target = browser.find_element_by_xpath('''//*[@id="searchPageResultsTabs"]/li[3]/a''')
-        news_target.click()
-        link = browser.current_url
-        links.append(link)
-    return links
+    browser.get(url)
+    search_form = browser.find_element_by_xpath('''/html/body/div[5]/header/div[1]/div/div[3]/div[1]/input''')
+    search_form.send_keys(name)
+    search_form.send_keys(Keys.ENTER)
+    sleep(2)
+    news_target = browser.find_element_by_xpath('''//*[@id="searchPageResultsTabs"]/li[3]/a''')
+    news_target.click()
+    sleep(2)
+    news = []
+    for i in range(20):
+        new_article = browser.find_element_by_xpath('''//*[@id="fullColumn"]/div/div[4]/div[3]/div/div[''' + str(i+1) + ''']/div/a''')
+        new_element = new_article.text
+        news.append(new_element)
+    dates = []
+    for i in range(20):
+        new_article = browser.find_element_by_xpath('''//*[@id="fullColumn"]/div/div[4]/div[3]/div/div[''' + str(i+1) + ''']/div/div/time''')
+        new_element = new_article.text
+        dates.append(new_element)
+    browser.close
+    data = []
+    for i in range(len(news)):
+        data.append((dates[i],news[i]))
+    return data
+
 
 def main():
-    names = ['Магнит', 'Мегафон', 'Газпром']
-    url='https://ru.investing.com/'
-    links = get_links(url,names)
-    for index, link in enumerate(links):
-        write_file(link,names[index], index)
+    names = ['Магнит', 'Газпром', 'Лукойл']
+    url = 'https://ru.investing.com/'
+    for name in names:
+        write_file(url, name)
+
 if __name__ == '__main__':
      main()
